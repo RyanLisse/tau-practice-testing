1dec5308337d2efb5b0561c4fb62d5afc8da605d
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2023-05-04 08:22:46 +0200

    Let's try the mob timer

---
+++ src/test/java/junit5/FirstTestClass.java
@@ -0,0 +1,14 @@
+package junit5;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTestClass {
+    @Test
+    void firstMethod(){
+        System.out.println("This is the first method");
+    }
+    @Test
+    void secondMethod(){
+        System.out.println("This is the Second Method");
+    }
+}
---
+++ src/test/java/taudemo/SeleniumTest.java
@@ -0,0 +1,22 @@
+package taudemo;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SeleniumTest {
+    @BeforeAll
+    public void beforeAll() {
+
+    }
+
+    @AfterAll
+    public void afterAll() {
+
+    }
+
+    @Test
+    public void openThePageAndCheckTheTitle() {
+
+    }
+}
